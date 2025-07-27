b7fd1af409916f4878739c9b5bf5313da51364da
Stephen Xie <zonghanxie@proton.me>
2025-07-27 20:15:24 +0800

    added themes and modify requirements.txt

--- conf.py
+++ conf.py
@@ -6,10 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Sphinx Test Documentation'
-copyright = '2025, Documentation Team'
-author = 'Documentation Team'
-release = '1.0.0'
+project = 'Zonghan Notebooks'
+copyright = '%Y Zong-han, Xie'
+author = 'Zong-han, Xie'
+release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -30,7 +30,14 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+import sphinx_pdj_theme
+#html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_pdj_theme'
+html_theme = 'sphinx_book_theme'
+#html_theme = 'press'
+#html_theme = 'groundwork'
+#html_theme = 'alabaster'
+#html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 html_static_path = ['_static']
 
 # -- MyST Parser Configuration -----------------------------------------------
